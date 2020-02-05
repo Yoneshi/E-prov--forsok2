8c094956e0cf9fcae90ca72b576d53e4afe9d99b
Yoneshi <treangpusri@gmail.com>
2020-02-05 10:16:43 +0100

    Försöker igen MIkael
    Hej

---
+++ HeroAca/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroAca
+{
+    class Hero
+    {
+        public Gender gender;
+        public string name;
+        public Breed breed;
+        public int age;
+
+
+        public Hero(Gender gender, string name, int age, Breed breed)
+        { 
+            this.gender = gender; // detta kodstycke refererar till gender variabeln utanför metoden och ger ett värde till klassens gender istället för att loopa internt.
+            this.name = name;
+            this.age = age;
+            this.breed = breed;
+
+
+        }
+        public void Print() //glöm inte att ha parenteser efter metoder
+        {
+
+            Console.WriteLine("Gender: " + gender.ToString() + "." + " " + "Name: " + name); // denna rad skriver ut kön och namn. för att convertera en enums måste man skriva ".ToString)
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Breed " + breed.ToString());
+        }
+        enum Gender // enum byter ut siffor till bokstäver, typ som man gjorde i minecraft lol
+        {
+            neutral,
+            Male,
+            Female
+
+        }
+       /* enum Breed
+        {
+            Human,
+            Mutant,
+            Emitter
+
+        kalle
+        }
+       */
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+
+           
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
